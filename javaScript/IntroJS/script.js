b6bae8a426036b8be1f6994c0543b5c4c9d8c19a
naman <namanpancholi1998@gmail.com>
2024-04-28 22:57:37 +0530

    java script intro added

---
+++ javaScript/IntroJS/script.js
@@ -0,0 +1,16 @@
+// alert("Älert");
+
+console.log("I am in Console Log");
+// console.error("I am Console Error");
+// console.warn("I am Console Warning");
+
+// let a = prompt("Enter Your Age");
+// console.log(a);
+
+// let isTrue = confirm("Are you sre You want to learn Java Script");
+
+// if (isTrue) {
+//   console.log("You can Do It");
+// } else {
+//   console.log("");
+// }

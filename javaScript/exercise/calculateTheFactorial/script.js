b4a904bac2aa3a778a83d1f48bd42854bd666f1d
naman <namanpancholi1998@gmail.com>
2024-04-30 00:20:23 +0530

    done

--- javaScript/exercise/calculateTheFactorial/script.js
+++ javaScript/exercise/calculateTheFactorial/script.js
@@ -7,7 +7,7 @@
 */
 
 // Using for loop
-// let num = prompt("Enter a nsumber");
+// let num = prompt("Enter a number");
 // let ans = 1;
 
 // for (let i = num; i > 0; i--) {
@@ -27,3 +27,22 @@
 //   return curr;
 // });
 // console.log(ans);
+
+// Using reduce
+let a = 7;
+
+function factorial(number) {
+  let arr = Array.from(Array(number + 1).keys());
+  let c = arr.slice(1).reduce((a, b) => a * b);
+  return c;
+}
+
+function facFor(number) {
+  let fac = 1;
+  for (let index = 1; index <= number; index++) {
+    fac = fac * index;
+  }
+  return fac;
+}
+console.log(factorial(a));
+console.log(facFor(a));

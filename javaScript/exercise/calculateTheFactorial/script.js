3cbe541f34252ef9c593d5fe9be14cb795852deb
naman <namanpancholi1998@gmail.com>
2024-04-29 19:40:47 +0530

    chalenge accepted

---
+++ javaScript/exercise/calculateTheFactorial/script.js
@@ -0,0 +1,29 @@
+/**
+    Write a program to calculate factorial of a number using reduce and using for loop
+
+    6?
+    6 = 6*5*4*3*2*1
+
+*/
+
+// Using for loop
+// let num = prompt("Enter a nsumber");
+// let ans = 1;
+
+// for (let i = num; i > 0; i--) {
+//   ans *= i;
+// }
+
+// console.log(ans);
+
+// let arr = [];
+// for (let i = 1; i <= num; i++) {
+//   arr.push(i);
+// }
+// // console.log(arr);
+
+// let ans = arr.reduce((curr, prev) => {
+//   curr *= prev;
+//   return curr;
+// });
+// console.log(ans);

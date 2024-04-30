915a89e8cc95c25fc056d3b6810ce79b409bb838
naman <namanpancholi1998@gmail.com>
2024-04-30 22:09:31 +0530

    done

---
+++ javaScript/eventsJs/script.js
@@ -0,0 +1,23 @@
+console.log("Events in Js");
+
+// Mouse Events
+// https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
+
+// Keyboard Events
+// https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+// https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
+
+// Form Events
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/reset_event
+
+// let btn = document.getElementById("btn");
+// btn.addEventListener("click", () => {
+//   //   alert("I was clicked");
+//   document.querySelector(".box").innerHTML = "<b>You are clicked by button</b>";
+// });
+
+// btn.addEventListener("contextmenu", () => {
+//   alert("Right click");
+// });

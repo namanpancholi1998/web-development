e5412bedf2030adc0bcb88375cb2258bcd91a463
naman <namanpancholi1998@gmail.com>
2024-07-08 00:13:54 +0530

    added next-auth.js

--- nextJs/authentication/app/page.js
+++ nextJs/authentication/app/page.js
@@ -1,9 +1,22 @@
-import Image from "next/image";
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
 
-export default function Home() {
+export default function Component() {
+  const { data: session } = useSession();
+  console.log(session);
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
   return (
-    <div>
-      <h1>Middleware in NextJS</h1>
-    </div>
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn("github")}>Sign in using Github</button>
+      <button onClick={() => signIn("google")}>Sign in using Google</button>
+    </>
   );
 }
